a36e68e8eefa26bfa9ce9ead15cb5c84ae134c54
imokech <choobeh77@gmail.com>
2022-06-21 12:54:39 +0430

    init  basic webserver

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	r := mux.NewRouter()
+
+	r.Handle("/", http.FileServer(http.Dir("./views")))
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("./static/"))))
+
+	http.ListenAndServe(":8090", r)
+}
